b8cb574112fc33ef04ac50e48c196b46d40cf58a
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-06 16:20:22 +0530

    Mood Analyser Class UC1

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,1 +1,17 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.*;
 
+public class MoodAnalyser {
+
+    public String analyseMood(String message){
+        String[] split = message.split(" ", 0);
+        for (String x: split){
+            if (x.equals("Sad")){
+                return "SAD";
+            }
+        }
+        return "HAPPY";
+     }
+
+}
