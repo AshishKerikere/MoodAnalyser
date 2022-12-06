cbdf9a8f1c8d7c4bfe9b5a03e1c400d5c2406015
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-06 16:19:28 +0530

    Main Method UC1

--- MainMethod.java
+++ MainMethod.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
+public class MainMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Message regarding Mood");
+        String message = scanner.nextLine();
 
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        System.out.println(moodAnalyser.analyseMood(message));
+    }
+}
